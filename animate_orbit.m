87cb81ebd4728d2474b9189e7f7fd8ecd92fb962
Finn Hansch <fhansch@stud.hs-bremen.de>
2022-02-01 09:05:57 +0100

    Add default settings and add user settings
    With this change user settings are loaded and replace the default values
    set in the default matrix.
    This enables the program to run even when not all settings are set.
    Also some code has been refactored and cleaned up.

--- animate_orbit.m
+++ animate_orbit.m
@@ -1,23 +1,35 @@
 % init
 %run('~/work/tools/XHPS_2021/HPS_simulation/hps_startup.m')
+close all;
 load('states.mat');
 
-%% settings
-vidName='vid2';
-
-% sim_time %TODO: warning
-sim_step = core_params.dt_sim ;
-t_start = datenum(HPS_convertMJD2CalendarDate(core_params.start_date(1)));
-
-% step includes only every step'th datapoint from states.mat to reduce filesize and rendering time
-step = 5;
-res = '1080p';
-view = [0 20];
+%% local settings
+vidName='jeb2';
 duration = 10; % seconds
-debug = 1;
+debug = 0;
 
-frameVec = generateFrames(states, step, sim_step, t_start, view, res, debug);
 
-if debug ~= 1
+%% settings
+init_parameters
+% sim_time %TODO: warning
+settings.sim_step = core_params.dt_sim ;
+settings.t_start = datenum(HPS_convertMJD2CalendarDate(core_params.start_date(1)));
+
+
+settings.step = 50; % step includes only every step'th datapoint from states.mat to reduce filesize and rendering time
+settings.resolution = '1080p';
+settings.viewAngle = [0 20];
+settings.camMode = 'fov';
+settings.satelliteModel = 'stl';
+settings.earthTransparency = 1;
+settings.zoom = 10e2;
+settings.progress = true;
+
+
+frameVec = generateFrames(states, settings, debug);
+
+if debug == 0
+	% Save frameVec to file system
+	save('frameVec','frameVec', '-v7.3')
 	saveVid(vidName, duration, frameVec)
 end
--- generateFrames.m
+++ generateFrames.m
@@ -1,23 +1,56 @@
-function aniVec = generateFrames(states, step, sim_step, t_start, angle, res, debug)
+function frameVec = generateFrames(states, settings, debug)
 
+%% Launching parpool for multithreadding
 if isempty(gcp('nocreate')) && debug == 0
    parpool;
 end
 
+%% Setting settings
+% Defining default values for unspecified user settings
+def = struct(	'step', 1, ...
+				'resolution', '1080p', ...
+				'sim_step', 10, ...
+				't_start', 730486, ...
+				'borderScale', 1.2, ...
+				'vecLength', 500, ... %TODO: factor of zoom
+				'camMode', 'fixed', ...
+				'zoom', 50, ...
+				'earthTransparency', 1, ...
+				'satelliteModel', 'none', ...
+				'viewAngle', [0 90], ...
+				'progress', 1, ...
+				'stlfactor', 1e-1, ...
+				'stlfile', 'res/jeb.stl', ...
+				'stlBaseColor', 'black', ...
+				'stlEdgeColor', 'green', ...
+				'earthfile', 'res/earth.jpg', ...
+				'earthPanels', 180 ...
+			);
+
+				%TODO: rotate image file
+%TODO: image_file loaded from 'http://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Land_ocean_ice_2048.jpg/1024px-Land_ocean_ice_2048.jpg';
+
+
+% Overwriting default settings with specified user settings
+diff = intersect(fieldnames(settings),fieldnames(def));
+for n = 1:length(diff)
+	def.(diff{n}) = settings.(diff{n});
+end
+
+%% Reading in states
+% only reading every stepth value of states
 endframe=length(states);
+state = states(1:def.step:endframe,:);
 
-% size of space around plot TODO: get from Vector lenght
-scale = 1.2;
+% optimizing for multithreadded processing
+vSatPos = state(:,11:13);
+vSatOri = state(:,4:7);
+vSatVel = state(:,8:10);
 
-state = states(1:step:endframe,:);
-state11= state(:,11);
-state12 = state(:,12);
-state13 = state(:,13);
-state47 = state(:,4:7);
-state810 = state(:,8:10);
+fv = stlread(def.stlfile);
 
-% Vid quality
-switch res
+%% Defining vido quality based on user settings
+switch def.resolution
 	case '720p'
 		width = 1280;
 		height = 720;
@@ -26,100 +59,99 @@ switch res
 		height = 1080;
 end
 
-zoom = 50;
-camMode = 'around';
-stlfactor = 1e-1;
-
-% impoted STL object
-fv = stlread('jeb.stl');
-
+%% DEBUG-mode
+% Display loaded stl file in figure (DEBUG-mode)
 if debug == 1
-    points=fv.Points*stlfactor;
-	jeb = trimesh(fv.ConnectivityList, points(:,1),points(:,2),points(:,3));
+    points=fv.Points*def.stlfactor;
+	ship = trimesh(fv.ConnectivityList, points(:,1),points(:,2),points(:,3));
+	set(ship, 'FaceColor', def.stlBaseColor, 'EdgeColor', def.stlEdgeColor);
 	view([0 0])
 	axis equal
-	%xdata = get(jeb,'xdata');
-	%ydata = get(jeb,'ydata');
-	%zdata = get(jeb,'zdata');
-	%figure
-	%jebweb = surf(xdata,ydata,zdata);
 end
 
-
+% Display video figure in DEBUG-mode
 if debug > 0
     fig = figure;
 else
     fig = figure('visible','off');
 end
 
-plot3(state11,state12,state13);
+%% Prepare looprun
+plot3(vSatPos(:,1),vSatPos(:,2),vSatPos(:,3))
 set(fig, 'Position',  [100, 100, width, height]);
 
-xl = xlim*scale;
-yl = ylim*scale;
+% scaling axes
+xl = xlim*def.borderScale;
+yl = ylim*def.borderScale;
 xlim(xl)
 ylim(yl)
-ie = floor(endframe/step)+1;
-if ie > length(states)
-	ie = length(states);
+
+% define last frame 
+ie = ceil(endframe/def.step);
+
+% initializing vector to save frames
+frameVec = struct('cdata', cell(1, ie), 'colormap', cell(1, ie));
+
+
+%% printing progress bar (multithread compatible)
+if def.progress
+	fprintf('Progress:\n');
+	fprintf(['\n' repmat('.',1,ie) '\n\n']);
 end
 
-
-m=ie;
-fprintf('Progress:\n');
-fprintf(['\n' repmat('.',1,m) '\n\n']);
-
-%aniVec(ie)=0;
-aniVec = struct('cdata', cell(1, ie), 'colormap', cell(1, ie));
-
-
-
+%% Main loop
 for i=1:ie
     clf
     
-    k=i*step - step+1;
-    tstr = datestr(t_start + (k-1) * seconds(sim_step));
-    t = t_start + (k-1) * sim_step;
+%% Calculate time
+	% Calculating index k from original states matrix
+    k=i*def.step - def.step;
+	% Calculating timestamp of current state by adding x*sim_step to the starttime
+    tstr = datestr(def.t_start + k * seconds(def.sim_step));
+    t = def.t_start + k * def.sim_step;
 
-	
-    % Plot orbit line
-    plot3(state11,state12,state13)
-    hold on
-    
-%% calculate satellite and camera position vectors
+%% Calculate satellite and camera position vectors
 	% velocity vector
-    velocity = [state810(i,1)*500,state810(i,2)*500,state810(i,3)*500];
+    velocity = [vSatVel(i,1)*def.vecLength,vSatVel(i,2)*def.vecLength,vSatVel(i,3)*def.vecLength];
 	vn = velocity./norm(velocity);
-	vfront = vn*zoom;
+	vfront = vn*def.zoom;
     % position of satelite
-    satPos = [state11(i) state12(i) state13(i)];
+    satPos = [vSatPos(i,1) vSatPos(i,2) vSatPos(i,3)];
     sn = satPos./norm(satPos);
-    satView = satPos+sn*zoom;
+    satView = satPos+sn*def.zoom;
 
-    orbPlaneA = [state11(1) state12(1) state13(1)] ;
-    orbPlaneB = [state11(2) state12(2) state13(2)] ;
-    orbPlaneC = [state11(3) state12(3) state13(3)] ;
+    orbPlaneA = [vSatPos(1,1) vSatPos(1,2) vSatPos(1,3)] ;
+    orbPlaneB = [vSatPos(2,1) vSatPos(2,2) vSatPos(2,3)] ;
+    orbPlaneC = [vSatPos(3,1) vSatPos(3,2) vSatPos(3,3)] ;
     orbNorm = cross(orbPlaneB-orbPlaneA,orbPlaneC-orbPlaneA);
+	clear orbPlaneA orbPlaneB orbPlaneC
     
+    % Plot orbit as blue line
+    plot3(vSatPos(:,1),vSatPos(:,2),vSatPos(:,3))
+    hold on
 
-%% plot satellite 
-		% 2D circle
-    %plot3(state11(i),state12(i),state13(i),'color', 'red', 'Marker', 'o', 'MarkerSize', 7 , 'linewidth',2)
+%% Plot satellite 
 
-		% 3D sphere
-    [x, y, z] = ellipsoid(state11(i), state12(i), state13(i), 50, 50, 50, 20);
-    shuttle=surf(x,y,-z, 'FaceColor', 'none', 'EdgeColor', 0.5*[1 1 1]);
-    %set(shuttle, 'FaceColor', 'texturemap', 'CData', cdata, 'FaceAlpha', alpha, 'EdgeColor', 'none');
+%% No sat
+	if strcmp(def.satelliteModel, 'none') 
+    plot3(satPos(1), satPos(2), satPos(3),'color', 'red', 'Marker', 'o', 'MarkerSize', 7 , 'linewidth',2)
 
-    %jeb = trimesh(fv);
-    points=fv.Points*stlfactor+satPos;
-	jeb = trimesh(fv.ConnectivityList, points(:,1),points(:,2),points(:,3));
-		%jeb = patch(fv,'FaceColor', [0.8 0.8 1.0], 'EdgeColor', 'none',        'FaceLighting',    'gouraud', 'AmbientStrength', 0.15);
-
+	% 3D sphere when no satellite
+    [x, y, z] = ellipsoid(satPos(1),satPos(2),satPos(3), 50, 50, 50, 20);
+    ship=surf(x,y,z, 'FaceColor', 'none', 'EdgeColor', 'none');
     
-    
+	elseif strcmp(def.satelliteModel, 'stl')
+%% STL sat
+    points=fv.Points*def.stlfactor+satPos;
+	ship = trimesh(fv.ConnectivityList, points(:,1),points(:,2),points(:,3));
+	set(ship, 'FaceColor', def.stlBaseColor, 'EdgeColor', def.stlEdgeColor);
+	%ship = patch(fv,'FaceColor', [0.8 0.8 1.0], 'EdgeColor', 'none',        'FaceLighting',    'gouraud', 'AmbientStrength', 0.15);
+	end
+
+
+%% Plot velocity Vec
     %plot velocity vector
-    quiver3(state11(i),state12(i),state13(i),state810(i,1)*500,state810(i,2)*500,state810(i,3)*500,'linewidth',2)
+    quiver3(satPos(1), satPos(2), satPos(3),velocity(1),velocity(2),velocity(3),'linewidth',2)
      
     %plot inertial kos
     a=6771000;
@@ -128,13 +160,13 @@ for i=1:ie
     y_axis= [0;axis_length;0];
     z_axis= [0;0;axis_length];
     
+	%TODO: inertial coordsys
     %quiver3(0,0,0,axis_length*1.5,0,0,'linewidth',3,'color', 'blue')
     %quiver3(0,0,0,0,axis_length*1.5,0,'linewidth',3,'color', 'red')
     %quiver3(0,0,0,0,0,axis_length*1.5,'linewidth',3,'color', 'green')
     
-    
     %% Plot body fixed frame
-    q_k = state47(i,:);
+    q_k = vSatOri(i,:);
     
     %transform inertial kos to body fixed by quaternion multiplication
     %(xrvar qnefgryyhat qrf Beovgny Xbs in eci daher vecbyquattransposed)
@@ -149,78 +181,79 @@ for i=1:ie
     %quiver3(state11(i),state12(i),state13(i),z_axis_body(1),z_axis_body(2),z_axis_body(3),'linewidth',2,'color', 'green')
     
     
-    %% plot earth
+%% Plot earth
     box off
-		axis vis3d off
+	axis vis3d off
     
     grs80 = referenceEllipsoid('grs80','m');
     
-    ax = axesm('globe','Geoid',grs80,'Grid','off', ...
-    	  'GLineWidth',1,'GLineStyle','-',...
-        'Gcolor','black','Galtitude',100);
+    ax = axesm(	'globe','Geoid',grs80,'Grid','off', ...
+				'GLineWidth',1,'GLineStyle','-', ...
+        		'Gcolor','black','Galtitude',100);
 
-    image_file = 'http://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Land_ocean_ice_2048.jpg/1024px-Land_ocean_ice_2048.jpg';
-% Load Earth image for texture map
+	% Load Earth image for texture map
+    image_file = def.earthfile;
     cdata = imread(image_file);
-    %cdata = load('Earth_fig_cdata');
-    alpha   = .8; % globe transparency level, 1 = opaque, through 0 = invisible
-    erad    = a ;%6371008.7714; % equatorial radius (meters)
-    prad    = a; %6371008.7714; % polar radius (meters)
-    npanels = 180;   % Number of globe panels around the equator deg/panel = 360/npanels
+	% globe transparency level, 1 = opaque, through 0 = invisible
 
-    [x, y, z] = ellipsoid(0, 0, 0, erad, erad, prad, npanels);
+	% rendering earth
+    [x, y, z] = ellipsoid(0, 0, 0, a, a, a, def.earthPanels);
     globe = surf(x,y,-z, 'FaceColor', 'none', 'EdgeColor', 0.5*[1 1 1]);
-    set(globe, 'FaceColor', 'texturemap', 'CData', cdata, 'FaceAlpha', alpha, 'EdgeColor', 'none');
-   % set(jeb, 'FaceColor', 'blue', 'EdgeColor', 'blue');
+    set(globe, 'FaceColor', 'texturemap', 'CData', cdata, 'FaceAlpha', def.earthTransparency, 'EdgeColor', 'none');
     
-    direction = [0 0 1];
-    origin = [0 0 0]; 
+
+%% Rotate Earth
     deg_rot = t/3600*15;
-
-    rotate(globe, direction, deg_rot, origin);
+    rotate(globe, [0 0 1], deg_rot, [0 0 0]);
    
- %% TODO: fix rotation. 
-%rotate(jeb, [1 0 0], 90, satPos);
+	%% TODO: enable rotation of ship. 
+	%rotate(ship, [1 0 0], 90, satPos);
 
+%% Set Viewpoint 
     campos('manual')
-    view(angle);
-    switch camMode 
+    view(def.viewAngle);
+    switch def.camMode 
+		case 'fixed'
 		case 'follow'
+			%TODO: add angle option
             camtarget(satPos)
-			camlookat(jeb)
+			camlookat(ship)
 			campos(satPos+vfront)
 			camup(satPos);
-		case 'around'
+		case 'fov'
+			%TODO: add angle option
             camtarget([0 0 0])
-            camlookat(jeb)
+            camlookat(ship)
 			campos(satView)
 			camup(orbNorm)
-            %camlookat(jeb)
 	end
+
+%% Optimize visuals
+
 	%TODO: Add a camera light, and tone down the specular highlighting
 	%camlight('headlight');
 	%material('dull');
     %shading faceted; 
-
     
+%% Equalize axes and Framesize
     xlim(xl);
     ylim(yl);
-    xlabel(tstr)
+    title(tstr);
     
-    if debug == 2
+
+%% Generate frame from current plot
+    if debug == 3
 		drawnow;
-    else if debug == 1
+    else if debug > 0
 		drawnow;
 		break;
 	else
-		aniVec(i) = getframe(fig, [0 0 width height]);
+		frameVec(i) = getframe(fig, [0 0 width height]);
 	end
     
-    fprintf('\b#\n');
-    
-end
-
-	if debug == 0
-		save('aniVec','aniVec', '-v7.3')
+	if def.progress
+		fprintf('\b#\n');
 	end
-end
+    
+end % forloop
+end % function

7b8acd99a991f0dcb3b38c5c71a5cb8f28d85a62
Finn Hansch <fhansch@stud.hs-bremen.de>
2022-02-08 17:23:52 +0100

    Document user settings and code

--- animate_orbit.m
+++ animate_orbit.m
@@ -1,35 +1,72 @@
-% init
-%run('~/work/tools/XHPS_2021/HPS_simulation/hps_startup.m')
-close all;
+%% initialize workspace
+% Making sure that XHPS toolbox is loaded (path needs to be adjusted accordingly)
+% This line can be omitted the hps_startupscript has been run manually or the path was added to Matlab by default
+run('<INSERT_PATH_HERE>/XHPS_2021/HPS_simulation/hps_startup.m')
+
+% Reading in state matrix from Simulation
+% This line can be ommitted when the states matrix already exists in the workspace 
+% However to be able to create a video different settings at a later time it is advisable to save the states matrix in a file by executing 
+% the following command: save('states.mat', 'states')
 load('states.mat');
 
 %% local settings
-vidName='jeb2';
-duration = 10; % seconds
+% Setting the output video name. CAREFUL!: This overwrites existing videofiles with that name on the same path
+videoName='orbit-animation';
+
+% Setting the video duration (shorter value results in quicker playback)
+% This setting should be balancend with the settings.step value below to ensure smooth rendering
+% It can be tweaked after the run when nessesary using the saveVid(<name>, <duration>, <frameVector>) function
+duration = 10; % [s]
+
+% Turning on the debug mode:
+% - 0 = off -> generating full video (might take some time)
+% - 1 = oneShot -> generates and displays the first frame (to check settings) and displays the loaded satellite Model if enabled
 debug = 0;
 
-
 %% settings
+% Run init_parameters to read in simulation parameters 
+% This line can be omitted when the values already exist in the workspace or are being set manually
 init_parameters
-% sim_time %TODO: warning
-settings.sim_step = core_params.dt_sim ;
-settings.t_start = datenum(HPS_convertMJD2CalendarDate(core_params.start_date(1)));
 
+% Setting the starttime and the dt of every simulation step to calculate the time (and the earth's rotation if enabled)
+settings.t_start = datenum(HPS_convertMJD2CalendarDate(core_params.start_date(1))); % format = [matlab datenum]
+settings.sim_step = core_params.dt_sim ; % [s]
 
+% Defining the stepwidth 
+% Smaller steps takes longer to compile and cause larger filesizes 
 settings.step = 50; % step includes only every step'th datapoint from states.mat to reduce filesize and rendering time
+
+% Setting the video resolution
 settings.resolution = '1080p';
+
+% Choosing the view perspective
+% - fixed  -> set a fixed view in the ECS coordinate system
+% - follow -> follow the satellite 
+% - fov	   -> field of view: fly above the satellite with a view direction to the center of eart
+settings.camMode = 'fixed';
+
+% Choosing the default view Angle of the scene. It only takes effect if the camMode = 'fixed'
 settings.viewAngle = [0 20];
-settings.camMode = 'fov';
-settings.satelliteModel = 'stl';
-settings.earthTransparency = 1;
+
+% Choosing the cameras proximity to the satellite
+% Can be negative but should be positive to keep the satellite in front of the camera
 settings.zoom = 10e2;
-settings.progress = true;
+
+% Loading a 3D model of the satellite
+% - none -> no satellite model displayed
+% - enr	 -> load Element-, Node-, and Reflexion coefficient table provided by the HPS library (path needs to be specified with settings.enrfile)
+% - stl  -> load a local stl file (path needs to be specified with settings.stlfile)
+settings.satelliteModel = 'stl';
 
 
 frameVec = generateFrames(states, settings, debug);
-
 if debug == 0
-	% Save frameVec to file system
+	% Save frameVec and video to file system
 	save('frameVec','frameVec', '-v7.3')
-	saveVid(vidName, duration, frameVec)
+	saveVid(videoName, duration, frameVec)
 end
+
+% To regenerate video with a different duration [s], simply run with the frameVec matrix in workspace
+% load('frameVec'); % to load the frame Vector into the workspace
+% saveVid('videoName', duration, frameVec) % to export the video
+

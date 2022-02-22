b1noc/XHPS_view

commit 903cd5d1975c9e05077d108bf52da45e397374db

Fix cut off earth with polar orbits
- fix #1
This fixes a bug where the earth was cut off and displayed as a slice
when choosing polar orbits. This is fixed by plotting the entire
ellipsoid before saving the axis limits for the frames.

toolbox/generateFrames.m
@@ -94,8 +94,11 @@ else
 end
 
 %% Prepare looprun
+r_earth=6771000;
 plot3(vSatPos(:,1),vSatPos(:,2),vSatPos(:,3))
 set(fig, 'Position',  [100, 100, width, height]);
+[x, y, z] = ellipsoid(0, 0, 0, r_earth, r_earth, r_earth, def.earthPanels);
+globe = surf(x,y,-z, 'FaceColor', 'none', 'EdgeColor', 0.5*[1 1 1]);
 
 % scaling axes
 xl = xlim*def.borderScale;
@@ -186,7 +189,7 @@ for i=1:ie
 	end
      
 %% Plot ECI
-	r_earth=6771000;
+
 
 	if def.showECI
 		axis_length = def.earthVecLength*r_earth; %length of coordinate system axis
@@ -215,7 +218,6 @@ for i=1:ie
 		z_axis= [0;0;axis_length];
 
 		%transform inertial kos to body fixed by quaternion multiplication
-		%(xrvar qnefgryyhat qrf Beovgny Xbs in eci daher vecbyquattransposed)
 		x_axis_body = HPS_transformVecByQuatTransposed(x_axis,q_k);
 		y_axis_body = HPS_transformVecByQuatTransposed(y_axis,q_k);
 		z_axis_body = HPS_transformVecByQuatTransposed(z_axis,q_k);

commit 0cafb7cb84fa786f4e49734631085ce42f1533a0

Fix cut off orbit with equatorial orbits
This bug was introduced by 903cd5d1975c9e05077d108bf52da45e397374db
fixing #1. It just reverted the behaviour working fine on polar orbits.
By adding a hold to keep the plotted content the figure keeps the size
in both axis.

toolbox/generateFrames.m
@@ -95,10 +95,11 @@ end
 
 %% Prepare looprun
 r_earth=6771000;
-plot3(vSatPos(:,1),vSatPos(:,2),vSatPos(:,3))
 set(fig, 'Position',  [100, 100, width, height]);
 [x, y, z] = ellipsoid(0, 0, 0, r_earth, r_earth, r_earth, def.earthPanels);
 globe = surf(x,y,-z, 'FaceColor', 'none', 'EdgeColor', 0.5*[1 1 1]);
+hold on
+plot3(vSatPos(:,1),vSatPos(:,2),vSatPos(:,3))
 
 % scaling axes
 xl = xlim*def.borderScale;